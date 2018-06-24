cbbe23092be9ce352d87f2e6e70c1be20c6c3085
EduardoSantosGit <eduardosantos058@gmail.com>
2018-06-24 18:58:00 -0300

    add method get info processor

---
+++ AgentOS/WinOSInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgentOS
+{
+    public class WinOSInformation
+    {
+        public static WinOSInfo GetOSInfo()
+        {
+            var winos = new WinOSInfo();
+
+            winos = GetInfoProcessor(winos);
+
+            return winos;
+        }
+
+        private static WinOSInfo GetInfoProcessor(WinOSInfo winos)
+        {
+            winos.ProcessorArchitecture = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE",
+                EnvironmentVariableTarget.Machine);
+            winos.ProcessorIden = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            winos.ProcessorLevel = Environment.GetEnvironmentVariable("PROCESSOR_LEVEL");
+            winos.ProcessorNumber = Environment.ProcessorCount.ToString();
+            return winos;
+        }
+
+
+
+    }
+}
