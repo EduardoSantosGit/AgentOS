e74786c7a780cdd6707804d4e05b593ab5da5431
eduardosantosgit <eduardosantos058@gmail.com>
2018-06-25 13:35:06 -0300

    add class global ex kernel 32bits

---
+++ AgentOS/GlobalKernelEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentOS
+{
+    public static class GlobalKernelEx
+    {
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPhysicallyInstalledSystemMemory(out long TotalMemoryInKilobytes);
+    }
+    
+}
