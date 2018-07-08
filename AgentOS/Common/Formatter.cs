01aa021fbced66012349be800f7f31081993751e
EduardoSantosGit <eduardosantos058@gmail.com>
2018-07-08 19:33:41 -0300

    add class formatter data

---
+++ AgentOS/Common/Formatter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentOS.Common
+{
+    public static class Formatter
+    {
+
+        public static void FormatSpeedNet(this string value)
+        {
+            var speed = Convert.ToInt64(value) / 8;
+
+            int KB = 1024;
+            int MB = KB * KB;
+            int GB = MB * KB;
+            long TB = (long)GB * KB;
+
+            if (speed >= TB)
+                value = $"{(speed / TB)}TB";
+            else if (speed >= GB)
+                value = $"{(speed / GB)}GB";
+            else if (speed >= MB)
+                value = $"{(speed / MB)}MB";
+            else if (speed >= KB)
+                value = $"{(speed / KB)}KB";
+            else
+                value = $"{(speed / KB)}Bytes";
+        }
+    }
+}
