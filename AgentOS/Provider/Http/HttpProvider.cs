5e60cef0757c12416752fd23e3e7e95af5e495eb
eduardosantosgit <eduardosantos058@gmail.com>
2018-07-10 18:56:38 -0300

    add init class http provider

---
+++ AgentOS/Provider/Http/HttpProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Text;
+
+namespace AgentOS.Provider.Http
+{
+    public class HttpProvider
+    {
+
+        private readonly string _urlBase;
+        private readonly HttpClient _httpClient;
+
+
+        public HttpProvider(string urlBase, HttpClient httpClient)
+        {
+            _urlBase = urlBase;
+            _httpClient = httpClient ?? new HttpClient();
+            _httpClient.BaseAddress = new System.Uri(_urlBase);
+        }
+
+        public string SendPostJson<T>(string url, T body)
+        {
+            //var content = new StringContent(JsonConvert.SerializeObject(), Encoding.UTF8, "application/json");
+            //var result = _httpClient.PostAsync(url, content).Result;
+            return null;
+        }
+
+    }
+}
