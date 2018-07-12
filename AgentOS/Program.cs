803403bc86d27f7a38b4dc51dceafc582e05dac7
eduardosantosgit <eduardosantos058@gmail.com>
2018-07-12 20:51:12 -0300

    add fluent and test jobs

--- AgentOS/Program.cs
+++ AgentOS/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CommandLine;
+using FluentScheduler;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +15,39 @@ namespace AgentOS
         public static void Main(string[] args)
         {
 
-            var st = new Startup();
+            JobManager.Initialize(new ScheduledJobRegistry());
 
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed<Options>(opts => st.StartInputFlowMonitor(opts))
-                .WithNotParsed<Options>((errs) => st.ErrorInput(errs));
+            // Wait for something
+            //Console.WriteLine("Press enter to terminate...");
+            Console.ReadLine();
+
+            // Stop the scheduler
+            //JobManager.StopAndBlock();
+
+            //var st = new Startup();
+
+            //Parser.Default.ParseArguments<Options>(args)
+            //    .WithParsed<Options>(opts => st.StartInputFlowMonitor(opts))
+            //    .WithNotParsed<Options>((errs) => st.ErrorInput(errs));
+        }
+
+        public class ScheduledJobRegistry : Registry
+        {
+            public ScheduledJobRegistry()
+            {
+                Schedule<MyJob>().ToRunNow().AndEvery(2).Seconds();
+            }
+        }
+
+        public class MyJob : IJob
+        {
+            public void Execute()
+            {
+
+
+                // Execute your scheduled task here
+                Console.WriteLine("The time is {0:HH:mm:ss}", DateTime.Now);
+            }
         }
 
     }
--- AgentOS/Startup.cs
+++ AgentOS/Startup.cs
@@ -1,45 +1,49 @@
 ﻿using AgentOS.Services;
 using CommandLine;
+using FluentScheduler;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace AgentOS
 {
+    public class MyRegistry : Registry
+    {
+        public MyRegistry()
+        {
+        }
+    }
+
     public class Startup
     {
 
         public void StartInputFlowMonitor(Options options)
         {
 
-            Console.WriteLine("Current time: {0}", DateTime.Now);
+            JobManager.Initialize(new MyRegistry());
+            JobManager.AddJob(() => Console.WriteLine("Late job!"), (s) => s.ToRunEvery(5).Seconds());
 
-            IObservable<long> observable = Observable.Timer(TimeSpan.FromSeconds(2));
+            //Console.WriteLine("Current time: {0}", DateTime.Now);
 
-            // Token for cancelation
-            CancellationTokenSource source = new CancellationTokenSource();
+            //IObservable<long> observable = Observable.Timer(TimeSpan.FromSeconds(2));
 
-            Console.WriteLine("Action started at: {0}", DateTime.Now);
+            //// Token for cancelation
+            //CancellationTokenSource source = new CancellationTokenSource();
 
-            // Create task to execute.
-            Task task = new Task(() => StartInputFlowMonitor(options));
+            //Console.WriteLine("Action started at: {0}", DateTime.Now);
 
-            // Subscribe the obserable to the task on execution.
-            observable.Subscribe(x => task.Start(), source.Token);
+            //// Create task to execute.
+            //Task task = new Task(() => StartInputFlowMonitor(options));
+
+            //// Subscribe the obserable to the task on execution.
+            //observable.Subscribe(x => task.Start(), source.Token);
 
             //if (options.Start)
             //{
             //    Task.Run(() => CronJob(options.Interval));
             //    var service = new WindowsService(options.UrlBase, options.EndPoint);
             //}
-
         }
 
-        
         public void TriggerSchedule(int time)
         {
 
